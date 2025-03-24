1daebfe148ed372216a07eb21378770bcbf96edf
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-24 21:56:46 +0530

    Ultimate QA SetUp

---
+++ src/test/java/ultimateQA/HandlingForms.java
@@ -0,0 +1,39 @@
+package ultimateQA;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HandlingForms {
+	
+	WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://ultimateqa.com/filling-out-forms/");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void formOne() {
+		
+	}
+	
+	@Test
+	public void fromTwo() {
+		
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	
+}
