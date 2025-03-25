59b5b8d0cf9c7d80a4085ab6c86afaaa7194afc7
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-25 06:19:57 +0530

    Internet Hwroku app final commit

--- src/test/java/internetHerokuApp/HandlingForm.java
+++ src/test/java/internetHerokuApp/HandlingForm.java
@@ -2,8 +2,11 @@ package internetHerokuApp;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -22,7 +25,14 @@ public class HandlingForm {
 
 	@Test
 	public void handlinForm() {
-
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("tomsmith");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("SuperSecretPassword");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		WebElement errorMessage = driver.findElement(By.xpath("//div[@id='flash']"));
+		if (errorMessage.isDisplayed()) {
+			System.out.println(errorMessage.getText());
+			Assert.assertTrue(true);
+		}
 	}
 
 	@AfterClass
