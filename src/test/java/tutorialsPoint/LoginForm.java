b28cbee284932c0527ffbfca674877f16bef228e
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-05-04 14:41:41 +0530

    Tutorials point setup

---
+++ src/test/java/tutorialsPoint/LoginForm.java
@@ -0,0 +1,34 @@
+package tutorialsPoint;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginForm {
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.tutorialspoint.com/selenium/practice/login.php");
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void Login() {
+		
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
---
+++ src/test/java/tutorialsPoint/PracticeForm.java
@@ -0,0 +1,34 @@
+package tutorialsPoint;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class PracticeForm {
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void Login() {
+		
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
