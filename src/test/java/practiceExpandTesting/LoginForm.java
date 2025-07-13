7bd9730c9c1701e028c3656fc1d26a197ee04a65
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-13 21:47:39 +0530

    Login Page Final Execution

--- src/test/java/practiceExpandTesting/LoginForm.java
+++ src/test/java/practiceExpandTesting/LoginForm.java
@@ -24,7 +24,7 @@ public class LoginForm {
 		driver.manage().window().maximize();
 	}
 
-//	@Test(priority = 1)
+	@Test(priority = 1)
 	public void loginFormValidCredens() {
 		WebElement userId = driver.findElement(By.xpath("//input[@id='username']"));
 		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
@@ -33,47 +33,59 @@ public class LoginForm {
 
 		userId.sendKeys("practice");
 		password.sendKeys("SuperSecretPassword!");
+		try {
+			driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+			System.out.println(driver.findElement(By.xpath("//div[@id='flash']//b")).getText());
 
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
-		System.out.println(driver.findElement(By.xpath("//div[@id='flash']//b")).getText());
+			driver.findElement(By.xpath("//a[@class='button secondary radius btn btn-danger']")).click();
 
-		driver.findElement(By.xpath("//a[@class='button secondary radius btn btn-danger']")).click();
-
-		Assert.assertEquals("Test Login Page for Automation Testin Practice", title);
+			Assert.assertEquals("Test Login Page for Automation Testin Practice", title);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Please check element's xpath");
+		}
 	}
 
-//	@Test(priority = 2)
-	public void loginFormInValidCredens()throws Exception {
+	@Test(priority = 2)
+	public void loginFormInValidCredens() throws Exception {
 		WebElement userId = driver.findElement(By.xpath("//input[@id='username']"));
 		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
 
 		userId.sendKeys("practice");
 		password.sendKeys("WrongPassword");
+		try {
+			driver.findElement(By.xpath("//button[@class=\"btn btn-bg btn-primary d-block w-100\"]")).click();
 
-		driver.findElement(By.xpath("//button[@class=\"btn btn-bg btn-primary d-block w-100\"]")).click();
-		
-		Thread.sleep(3000);
-		
-		String resultMessage = driver.findElement(By.xpath("//div[@id='flash']//b")).getText();
-		System.out.println(resultMessage);
-		Assert.assertEquals("Your password is invalid!", resultMessage);
+			Thread.sleep(3000);
+
+			String resultMessage = driver.findElement(By.xpath("//div[@id='flash']//b")).getText();
+			System.out.println(resultMessage);
+			Assert.assertEquals("Your password is invalid!", resultMessage);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Please check element's xpath");
+		}
 	}
 
 	@Test(priority = 3)
-	public void loginFormInValidCredens2()throws Exception {
+	public void loginFormInValidCredens2() throws Exception {
 		WebElement userId = driver.findElement(By.xpath("//input[@id='username']"));
 		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
 
 		userId.sendKeys("WrongUserName");
 		password.sendKeys("SuperSecretPassword!");
-		
-		driver.findElement(By.xpath("//button[@class='btn btn-bg btn-primary d-block w-100']")).click();
-		
-		Thread.sleep(3000);
-		
-		String resultMessage = driver.findElement(By.xpath("//div[@id='flash']//b")).getText();
-		System.out.println(resultMessage);
-		Assert.assertEquals("Your username is invalid!", resultMessage);
+		try {
+			driver.findElement(By.xpath("//button[@class=\"btn btn-bg btn-primary d-block w-100\"]")).click();
+
+			Thread.sleep(3000);
+
+			String resultMessage = driver.findElement(By.xpath("//div[@id='flash']//b")).getText();
+			System.out.println(resultMessage);
+			Assert.assertEquals("Your username is invalid!", resultMessage);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Please check element's xpath");
+		}
 	}
 
 	@AfterClass
